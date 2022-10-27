3893c37a7df4ab2d618c6769c260584930147f32
rmk-s <karimsebb93@gmail.com>
2022-10-27 21:41:09 +0200

    Update ListOfBar.jsx

--- src/pages/ListOfBar.jsx
+++ src/pages/ListOfBar.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import Card from "../components/Card/Card";
 
@@ -6,28 +6,21 @@ const ListOfBar = () => {
 
     const [bars, setBars] = useState([]);
     
-    const fetchBars = async (quantity) => {
-        const response = await fetch(`https://api.openbrewerydb.org/breweries/random?size=5`);
+    const fetchBars = async () => {
+        const response = await fetch(`https://api.openbrewerydb.org/breweries/random?size=9`);
         const bars = await response.json();
         setBars(bars);
     }
 
-    useEffect(() => {
-        fetchBars();
-    }, [])
+    useEffect(() => { fetchBars(); }, [])
 
-    console.log(bars);
     return (
         <div className="App">
             
             <Link to='/' > Go to Home</Link>
 
             <div className='cardsContainer'>
-            {bars && bars.map((bar, index) => <div> <Link to={'barInformation/' + bar.id} key={bar.id} > <Card format = "large"
-    color = "white"
-    title = {bar.name}
-    tag = {bar.country}
-    description = "description of the bar or his adress"/> </Link> <br/> </div>)}
+                {bars && bars.map((bar, index) => <div> <Link to={'barInformation/' + bar.id} key={bar.id} > <Card format = "large" color = "white" title = {bar.name} tag = {bar.country} description = "description of the bar or his adress"/> </Link> <br/> </div>)}
             </div>
         </div>
     );
