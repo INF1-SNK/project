64e9a533c9612aadb3040dbf11baee3e917a6acc
Nicolas Raharinosy <mamilala.raharinosy@etu.univ-paris1.fr>
2022-10-27 23:42:00 +0200

    feat(barPresentationPage): header navbar footer and details added

--- src/pages/BarPresentation.jsx
+++ src/pages/BarPresentation.jsx
@@ -2,6 +2,13 @@ import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import Card from "../components/Card/Card";
 import { useParams } from 'react-router-dom';
+import { Header } from "../components/Header/Header";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import Text from "../components/Text/Text";
+import './style/barPresentation.css'
+
+//import Link from "../components/Link/Link";
 
 
 const BarPresentation = () => {
@@ -30,21 +37,23 @@ const BarPresentation = () => {
 //Il faut recuperer le bon bar et les données de celui sur lequel on a cliqué pour les mettre dans la cards 
 
     return (
-        <div>
-
+       <>
+            <Header></Header>
+            <Navbar menu={[
+                <Link to="/ListOfBar">Access to list of bars </Link>,
+                <Link to="/contactUs"> Contact Us PAGE </Link>
+                ]}></Navbar>
             <h1>Bar presentation  </h1>
 
-            <div className='cardsContainer'>
-                 <Card format = "large" color = "white" title = "{bar.name}" tag = "{bar.country}}" description = "description of the bar or his adress"/>
+            <div className='single-bar-card'>
+                <Card format = "large" color = "white" title = {bar.name} tag = {bar.country} description = {"Address : " + bar.street+ ", " + bar.city + (bar.state != null ? ", " + bar.state : "")}/>
+                <div>
+                    <Text content={"Phone number : " + bar.phone}></Text>
+                    <a href={bar.website_url}>{bar.website_url}</a>
+                </div>
             </div>
-
-            <Link to="/ListOfBar"> Acess to list of bars </Link>
-            <br/>
-            <button onClick={() => navigate('/')}> Go Home </button>
-            <br/> <br/>
-            <Card format = "large" color = "white" title = "Name of the bar" tag = "Contrie" description = "description of the bar or his adress" />
-
-        </div>
+            <Footer></Footer>
+        </>
     );
 }
 
