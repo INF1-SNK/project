d5d9f6ae239b7b90c394f1486fe9f7368caf0fac
Sofiane <sofiane.djemaa93@gmail.com>
2022-10-27 17:04:45 +0200

    feat(ContactUs) : Beginning implementation of the Contact page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,15 +1,23 @@
 import { Link, useNavigate } from "react-router-dom";
+import { Header }  from "../components/Header/Header";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const ContactUs = () => {
     const navigate = useNavigate();
 
     return (
+        <>
         <div>
+            <Header></Header>
+            <Navbar menu={[<Link to="/ListOfBar">Access to list of bars </Link>,<button onClick={() => navigate('/contactUs')}> Contact Us PAGE </button>]}></Navbar>
             <h1>Contact </h1>
             <Link to="/ListOfBar"> Acess to list of bars </Link>
             <br/>
             <button onClick={() => navigate('/')}> Go Home </button>
+            <Footer></Footer>
         </div>
+        </>
     );
 }
 
